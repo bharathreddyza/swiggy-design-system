c5c2ea92e91d2c0869041eca86459f7eaa5d9f5b
bharathreddza <bharathreddyza@gmail.com>
2021-04-26 23:02:11 +0530

    done

--- src/components/Sidebar/Sidebar.stories.js
+++ src/components/Sidebar/Sidebar.stories.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../../index.css'
-
+import {Button} from '../Button/Button'
 import Sidebar from './Sidebar';
 
 
 export default {
     component: Sidebar,
-    title: 'Sidebat',
+    title: 'Sidebar ',
   };
   
 //👇 We create a “template” of how args map to rendering
-const Template = (args) => <LocationSearcher {...args} />;
+const Template = (args) => <div>
+  <Sidebar   {...args} >
+    {/* console.log(props)
+  <button onClick={ args.slider=false}>evrbrbrb</button> */}
+
+  </Sidebar>
+
+
+</div>
+
+ 
+  function sidebar(args) {
+  const [open,setOpen] = useState(true)
+  return (
+    <>
+    <Sidebar open ={open}  setOpen= {setOpen} /> 
+    <Button label1="LOGIN" size="text-sm" type="primary"  onClick={()=>setOpen(!open)}/>
+    </>
+
+    
+  )
+}
 
 //👇 Each story then reuses that template
-export const One = Template.bind({});
-One.args = {  label: 'order' };
+export const One = sidebar.bind({});
+One.args = {  slider: false };
+
+
+
+
